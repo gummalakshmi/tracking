3bb313fe2398178548f41497f5cdce24e3c93e10
Ravi kasa <ravikumarkasa@gurus-imac.local>
2019-01-08 15:23:21 +0530

    Initial Commit

---
+++ praticeTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  praticeTracking
+//
+//  Created by Ravi kumar kasa on 08/01/19.
+//  Copyright © 2019 Megaroy solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
